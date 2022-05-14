ed332853915634a0a7de8560806a638a05a6d350
Tkamunya1 <titus.kamunya@student.moringaschool.com>
2022-05-14 19:28:34 +0300

    this commit adds the third question to index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const quizData = [
 
     {
-           question: "Which one is true?",
-           a: "HTML is a markup language",
-           b: "CSS is a programming language",
-           c: "You cannot use HTML without a separate CSS file",
-           d: "You need to be good at math to ace programming",
-           correct: "a",
-       },
-       {
+        question: "Which one is true?",
+        a: "HTML is a markup language",
+        b: "CSS is a programming language",
+        c: "You cannot use HTML without a separate CSS file",
+        d: "You need to be good at math to ace programming",
+        correct: "a",
+    },
+    {
         question: "Which of the following statements is false?",
         a: "Javascript is a programming language",
         b: "Javascript is the same as Java",
@@ -16,5 +16,13 @@ const quizData = [
         d: "Javascript is responsible for running a web browser",
         correct: "b",
     },
+    {
+        question: "Which of the following is not a programming language?",
+        a: "Javascript",
+        b: "CSS",
+        c: "PHP",
+        d: "Python",
+        correct: "b",
+    },
 
-    ]   
+]
