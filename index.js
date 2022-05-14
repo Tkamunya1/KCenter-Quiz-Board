9982420854ebe2b71450e594e869be1ee4de3108
Tkamunya1 <titus.kamunya@student.moringaschool.com>
2022-05-14 19:27:01 +0300

    this commit adds the second question to index.js

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ const quizData = [
            d: "You need to be good at math to ace programming",
            correct: "a",
        },
+       {
+        question: "Which of the following statements is false?",
+        a: "Javascript is a programming language",
+        b: "Javascript is the same as Java",
+        c: "Javascript has different frameworks",
+        d: "Javascript is responsible for running a web browser",
+        correct: "b",
+    },
+
     ]   
