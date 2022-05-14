310f2ef34bf075fc7d2bbff314c861023c6e3ac8
Tkamunya1 <titus.kamunya@student.moringaschool.com>
2022-05-14 19:25:22 +0300

    this commit will add the first question to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const quizData = [
+
+    {
+           question: "Which one is true?",
+           a: "HTML is a markup language",
+           b: "CSS is a programming language",
+           c: "You cannot use HTML without a separate CSS file",
+           d: "You need to be good at math to ace programming",
+           correct: "a",
+       },
+    ]   
